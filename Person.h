d3ff4695ce8d80d7ec8a8790f53f9c3b8620834e
youssefahmed16009528 <youssef0ahmed678@gmail.com>
2025-11-11 09:48:33 +0200

    Update Person.h

--- Person.h
+++ Person.h
@@ -5,7 +5,17 @@
 using namespace std;
 
 //#write Person class here
-
+class Person {
+    string name;
+    int id;
+  public :
+   Person(string n, int i){
+       name = n;
+       id = i;
+   }
+    void display(){
+         cout<<"Name: "<<name<<", ID: "<<id<<endl;
+    }
 
 
 
