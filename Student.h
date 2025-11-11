b465353569726b668255cfc4a7ade97d44392f38
youssefahmed16009528 <youssef0ahmed678@gmail.com>
2025-11-11 09:49:15 +0200

    Implement Student class with constructor and display method

--- Student.h
+++ Student.h
@@ -5,7 +5,18 @@
 #include <string>
 #using namespace std;
 //#write student class here
-
+ class Student :
+  int yearLevel;
+  string major;
+    public :
+     Student(string n, int i, int majorField, int yL) : Person(n,i){
+         major = majorField;
+         yearLevel = yL;
+     }
+     void display(){
+         Person::display();
+         cout<<"Major: "<<major<<", Year Level: "<<yearLevel<<endl;
+     }
 
 
 
