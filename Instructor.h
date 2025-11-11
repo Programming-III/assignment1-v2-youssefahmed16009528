26378a3a4796260849b981629f149921f6998656
youssefahmed16009528 <youssef0ahmed678@gmail.com>
2025-11-11 09:47:32 +0200

    Add Instructor class with constructor and display method

--- Instructor.h
+++ Instructor.h
@@ -4,7 +4,18 @@
 #include "Person.h"
 using namespace std;
 //#write Instructor class here
-
+class Instsructor :
+   string department;
+   int experienceYears;
+    public :
+     Instructor(string n, int i, string dept, int expY) : Person(n,i){
+         department = dept;
+         experienceYears = expY;
+     }
+     void display(){
+         Person::display();
+         cout<<"Department: "<<department<<", Experience Years: "<<experienceYears<<endl;
+        }
 
 
 
